06db2fe49cc278fcc5a9d598b325603a485c438b
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2024-11-15 18:12:12 -0500

    added backend and frontend directories

---
+++ backend/src/main/java/com/cannabase/CannabaseApplication.java
@@ -0,0 +1,13 @@
+package com.cannabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CannabaseApplication.class, args);
+	}
+
+}
