5ddfedff5a18ddc6643e9c0af661a271ee275a30
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2024-11-15 22:08:35 -0500

    added @EnableJpaRepositories(basePackages = "com.cannabase.repositories")

--- backend/src/main/java/com/cannabase/CannabaseApplication.java
+++ backend/src/main/java/com/cannabase/CannabaseApplication.java
@@ -2,10 +2,12 @@ package com.cannabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.cannabase")
+@EntityScan(basePackages = "com.cannabase.models")
+@EnableJpaRepositories(basePackages = "com.cannabase.repositories")
 public class CannabaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CannabaseApplication.class, args);
