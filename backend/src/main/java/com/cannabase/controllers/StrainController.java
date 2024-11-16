11ece13933a43f30d4e2b78345e770690a95c620
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2024-11-15 22:09:23 -0500

    updated controllers

--- backend/src/main/java/com/cannabase/controllers/StrainController.java
+++ backend/src/main/java/com/cannabase/controllers/StrainController.java
@@ -2,9 +2,6 @@ package com.cannabase.controllers;
 
 import com.cannabase.models.Strain;
 import com.cannabase.services.StrainService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -12,30 +9,48 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/strains")
 public class StrainController {
+    
     private final StrainService strainService;
-
-    @Autowired
+    
     public StrainController(StrainService strainService) {
         this.strainService = strainService;
     }
-
+    
     @GetMapping
-    public ResponseEntity<Page<Strain>> getAllStrains(Pageable pageable) {
-        return ResponseEntity.ok(strainService.getAllStrains(pageable));
+    public ResponseEntity<List<Strain>> getAllStrains() {
+        return ResponseEntity.ok(strainService.getAllStrains());
     }
-
+    
     @GetMapping("/{id}")
     public ResponseEntity<Strain> getStrainById(@PathVariable Long id) {
-        return ResponseEntity.ok(strainService.getStrainById(id));
+        return strainService.getStrainById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
-
+    
     @GetMapping("/search")
-    public ResponseEntity<List<Strain>> searchStrains(@RequestParam String query) {
-        return ResponseEntity.ok(strainService.searchStrains(query));
+    public ResponseEntity<List<Strain>> searchStrains(@RequestParam String name) {
+        return ResponseEntity.ok(strainService.searchStrainsByName(name));
     }
-
+    
     @PostMapping
     public ResponseEntity<Strain> createStrain(@RequestBody Strain strain) {
         return ResponseEntity.ok(strainService.createStrain(strain));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Strain> updateStrain(@PathVariable Long id, @RequestBody Strain strain) {
+        return ResponseEntity.ok(strainService.updateStrain(id, strain));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStrain(@PathVariable Long id) {
+        strainService.deleteStrain(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @GetMapping("/type/{typeName}")
+    public ResponseEntity<List<Strain>> getStrainsByType(@PathVariable String typeName) {
+        return ResponseEntity.ok(strainService.getStrainsByType(typeName));
     }
 }
--- backend/src/main/java/com/cannabase/controllers/StrainTypeController.java
+++ backend/src/main/java/com/cannabase/controllers/StrainTypeController.java
@@ -2,7 +2,6 @@ package com.cannabase.controllers;
 
 import com.cannabase.models.StrainType;
 import com.cannabase.services.StrainTypeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -10,25 +9,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/strain-types")
 public class StrainTypeController {
+    
     private final StrainTypeService strainTypeService;
-
-    @Autowired
+    
     public StrainTypeController(StrainTypeService strainTypeService) {
         this.strainTypeService = strainTypeService;
     }
-
+    
     @GetMapping
-    public ResponseEntity<List<StrainType>> getAllTypes() {
-        return ResponseEntity.ok(strainTypeService.getAllTypes());
+    public ResponseEntity<List<StrainType>> getAllStrainTypes() {
+        return ResponseEntity.ok(strainTypeService.getAllStrainTypes());
     }
-
+    
     @GetMapping("/{id}")
-    public ResponseEntity<StrainType> getTypeById(@PathVariable Long id) {
-        return ResponseEntity.ok(strainTypeService.getTypeById(id));
+    public ResponseEntity<StrainType> getStrainTypeById(@PathVariable Long id) {
+        return strainTypeService.getStrainTypeById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
-
+    
     @PostMapping
-    public ResponseEntity<StrainType> createType(@RequestBody StrainType strainType) {
-        return ResponseEntity.ok(strainTypeService.createType(strainType));
+    public ResponseEntity<StrainType> createStrainType(@RequestBody StrainType strainType) {
+        return ResponseEntity.ok(strainTypeService.createStrainType(strainType));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<StrainType> updateStrainType(@PathVariable Long id, @RequestBody StrainType strainType) {
+        return ResponseEntity.ok(strainTypeService.updateStrainType(id, strainType));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStrainType(@PathVariable Long id) {
+        strainTypeService.deleteStrainType(id);
+        return ResponseEntity.noContent().build();
     }
 }
