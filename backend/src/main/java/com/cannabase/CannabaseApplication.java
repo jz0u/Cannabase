f8676ca19103a30c3c7a22993b19d95eb5d403fd
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2024-11-15 19:37:02 -0500

    removed unnessarary imports, added componentScan to main app file (CannabaseApplication.java)

--- backend/src/main/java/com/cannabase/CannabaseApplication.java
+++ backend/src/main/java/com/cannabase/CannabaseApplication.java
@@ -1,13 +1,13 @@
 package com.cannabase;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cannabase")
 public class CannabaseApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CannabaseApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CannabaseApplication.class, args);
+    }
 }
